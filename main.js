20c489d6711e1e0c34bf78c24d2a399893e00499
Hoa Lam <78774658+lamnhuthoa@users.noreply.github.com>
2021-05-28 15:59:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+//DICE 1
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute('src', randomImageSource1);
+
+
+//DICE 2
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;    
+
+var image2 = document.querySelectorAll("img")[1];
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+image2.setAttribute('src', randomImageSource2);
+
+//WINNER!
+if (randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = "PLayer 1 Wins";
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector('h1').innerHTML = "PLayer 2 Wins";
+} else {
+    document.querySelector('h1').innerHTML = "Draw!";
+}
